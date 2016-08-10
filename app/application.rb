5fe6bd3d5e317f6865315d0d920ad645eae687d5
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-10 16:30:09 -0400

    finished get params with rack lab

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,22 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length > 0
+        @@cart.collect do |item|
+          resp.write "#{item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+    elsif req.path.match(/add/)
+      item_request = req.params["item"]
+      if @@items.include?(item_request)
+        @@cart << item_request
+        resp.write "added #{item_request}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
